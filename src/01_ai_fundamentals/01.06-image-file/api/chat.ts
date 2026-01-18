da379628e1794ab64e4eff4d1931b75e02547a17
khalidkhankakar <khalidkhankakar2468@gmail.com>
2026-01-18 12:33:27 +0500

    IMAGE AND FILES 06 🗄

---
+++ src/01_ai_fundamentals/01.06-image-file/api/chat.ts
@@ -0,0 +1,33 @@
+import { googleModel, groqModel } from '#src/model.ts';
+
+import {
+  convertToModelMessages,
+  createUIMessageStreamResponse,
+  streamText,
+  type ModelMessage,
+  type UIMessage,
+} from 'ai';
+
+
+export const POST = async (req: Request): Promise<Response> => {
+  const body = await req.json();
+
+  // TODO: get the UIMessage[] from the body
+  const messages: UIMessage[] = body.messages;
+
+  // TODO: convert the UIMessage[] to ModelMessage[]
+  const modelMessages: ModelMessage[] = await convertToModelMessages(messages)
+
+  // TODO: pass the modelMessages to streamText
+  const streamTextResult = streamText({
+    model: googleModel,
+    prompt: modelMessages
+  });
+
+  // TODO: create a UIMessageStream from the streamTextResult
+  const stream = streamTextResult.toUIMessageStream();
+
+  return createUIMessageStreamResponse({
+    stream,
+  });
+};
